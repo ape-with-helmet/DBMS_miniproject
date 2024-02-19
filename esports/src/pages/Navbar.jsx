3949c956e001186af6004cd50673d6b877b0cfa0
Sadhgun <ganeshshatrugna@gmail.com>
2024-02-20 02:39:53 +0530

    halkf done

--- esports/src/pages/Navbar.jsx
+++ esports/src/pages/Navbar.jsx
@@ -1,21 +1,23 @@
 import '../css/Navbar.css'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Logo from './buffalo-bills-logo.png'
 
 function NavBar() {
+    const location = useLocation();
     return (
         <>
             <nav className="navbar">
-                <div className="navbar-logo">
-                    <Link to="/"><img src={Logo} alt="Logo" className="navbar-image" /></Link>
+                <Link to="/" className='links'><div className="navbar-logo">
+                    <img src={Logo} alt="Logo" className="navbar-image" />
                     <span className="navbar-text">Ghost Esports</span>
-                </div>
+                </div></Link>
+                {location.pathname !== '/' && <button className='back-button' onClick={()=>window.history.back()}>Back</button>}
                 <ul className="navbar-links">
-                    <li className="navbar-links"><Link to="/">Add info</Link></li>
-                    <li className="navbar-links"><Link to="/team_info">Teams</Link></li>
-                    <li className="navbar-links"><Link to="/games">Games</Link></li>
+                    <li className="navbar-links-1"><Link to="/">Add info</Link></li>
+                    <li className="navbar-links-2"><Link to="/team_info">Teams</Link></li>
+                    <li className="navbar-links-3"><Link to="/games">Games</Link></li>
                 </ul>
             </nav>
         </>
