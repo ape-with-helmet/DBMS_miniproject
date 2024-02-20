8914624056658d5d6707b643a9ab7d3c3db3935a
Sadhgun <ganeshshatrugna@gmail.com>
2024-02-20 12:06:08 +0530

    fixed back button

--- esports/src/pages/Navbar.jsx
+++ esports/src/pages/Navbar.jsx
@@ -1,7 +1,6 @@
 import '../css/Navbar.css'
 import { Link, useLocation } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import back from './back.svg'
 import Logo from './buffalo-bills-logo.png'
 
 function NavBar() {
@@ -13,7 +12,7 @@ function NavBar() {
                     <img src={Logo} alt="Logo" className="navbar-image" />
                     <span className="navbar-text">Ghost Esports</span>
                 </div></Link>
-                {location.pathname !== '/' && <button className='back-button' onClick={()=>window.history.back()}>Back</button>}
+                {location.pathname !== '/' && <a className='back-button' onClick={()=>window.history.back()}><img src={back}/></a>}
                 <ul className="navbar-links">
                     <li className="navbar-links-1"><Link to="/">Add info</Link></li>
                     <li className="navbar-links-2"><Link to="/team_info">Teams</Link></li>
@@ -23,5 +22,7 @@ function NavBar() {
         </>
     )
 }
-
+{/* <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
+<circle cx="50" cy="50" r="40" fill="blue" /> */}
+// </svg>
 export default NavBar
